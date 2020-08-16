86f14dffb8d2037a4f80ea38cfe0d31f25371c53
Mario D Flores <43660822+SlimRunner@users.noreply.github.com>
2020-08-15 23:24:24 -0700

    initial commit

---
+++ src/test-update.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name     	GitHubDirectUpdate
+// @namespace	slidav.Desmos
+// @version  	0.0.1
+// @author		SlimRunner (David Flores)
+// @description	TM update test
+// @grant    	none
+// @match			https://*.desmos.com/calculator*
+// @downloadURL	https://gist.github.com/SlimRunner/aacc9cea998a3a8da31eae9d487412d7/raw/dgc-art-tools.user.js
+// @updateURL	https://gist.github.com/SlimRunner/aacc9cea998a3a8da31eae9d487412d7/raw/dgc-art-tools.user.js
+// ==/UserScript==
+
+(function() {
+	'use strict';
+	console.log(`My version is ${0}.${0}.${1}`);
+}());
